4c761eceaf9f6820c7f5da741deac0e4ca281509
Arko <arkoroyb@gmail.com>
2022-11-09 01:26:25 +0600

    mongodb connection implemented

--- index.js
+++ index.js
@@ -1,14 +1,43 @@
 const express = require('express')
 const cors = require('cors');
+const { MongoClient, ServerApiVersion } = require('mongodb');
 
 
 
-const port = process.env.PORT || 5000;
 const app = express()
+const port = process.env.PORT || 5000;
 
 
 app.use(cors());
 app.use(express.json());
+
+
+// const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASSWORD}@cluster0.et115mk.mongodb.net/?retryWrites=true&w=majority`;
+const uri = `mongodb+srv://cookingUser:Jc9K1yPHlzAjq9MM@cluster0.et115mk.mongodb.net/?retryWrites=true&w=majority`;
+const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true, serverApi: ServerApiVersion.v1 });
+
+
+async function run() {
+    try {
+        const serviceCollection = client.db('cookingCorner').collection('services')
+
+        app.get('/services', async (req, res) => {
+            const query = {}
+            const cursor = serviceCollection.find(query);
+            const services = await cursor.toArray();
+            res.send(services)
+        })
+    }
+    finally {
+
+    }
+}
+
+
+run().catch(err => console.log(err))
+
+
+
 
 
 app.get('/', (req,res) => {
