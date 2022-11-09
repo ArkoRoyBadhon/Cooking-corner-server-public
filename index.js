97bc197da45c22a3080c4da21513afee15dca404
Arko <arkoroyb@gmail.com>
2022-11-09 23:48:02 +0600

    post method implemented for add new service

--- index.js
+++ index.js
@@ -79,6 +79,12 @@ async function run() {
             res.send(result)
         })
 
+        app.post('/add-service', async (req,res) => {
+            const service = req.body;
+            const result = await serviceCollection.insertOne(service)
+            res.send(result);
+        })
+
 
 
 
