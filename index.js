bcce7edb1f1d276b006fc4259d9c4b8237c6aed4
Arko <arkoroyb@gmail.com>
2022-11-10 01:24:43 +0600

    dotenv implemented

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
 const express = require('express')
 const cors = require('cors');
 const { MongoClient, ServerApiVersion, ObjectId } = require('mongodb');
+require('dotenv').config()
 
 
 
@@ -12,8 +13,7 @@ app.use(cors());
 app.use(express.json());
 
 
-// const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASSWORD}@cluster0.et115mk.mongodb.net/?retryWrites=true&w=majority`;
-const uri = `mongodb+srv://cookingUser:Jc9K1yPHlzAjq9MM@cluster0.et115mk.mongodb.net/?retryWrites=true&w=majority`;
+const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASSWORD}@cluster0.et115mk.mongodb.net/?retryWrites=true&w=majority`;
 const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true, serverApi: ServerApiVersion.v1 });
 
 
