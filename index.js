2261b12ba31370ede9a2a4db4e9d8f18b4929fb9
Arko <arkoroyb@gmail.com>
2022-11-10 13:25:39 +0600

    jwt functionality added

--- index.js
+++ index.js
@@ -2,6 +2,7 @@ const express = require('express')
 const cors = require('cors');
 const { MongoClient, ServerApiVersion, ObjectId } = require('mongodb');
 require('dotenv').config()
+const jwt = require('jsonwebtoken');
 
 
 
@@ -17,10 +18,34 @@ const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASSWORD}@clu
 const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true, serverApi: ServerApiVersion.v1 });
 
 
+function verifyJWT(req,res, next) {
+    const authHeader = req.headers.authorization;
+    if (!authHeader) {
+        return res.status(401).send({message: 'unauthorized accesss'})
+    }
+    const token = authHeader.split(' ')[1] 
+    jwt.verify(token, process.env.ACCESS_TOKEN_SECRET, function(err, decoded){
+        if(err) {
+            res.status(403).send({message: 'forbidden access'})
+        }
+        req.decoded = decoded;
+        next();
+    })
+} 
+
+
+
 async function run() {
     try {
         const serviceCollection = client.db('cookingCorner').collection('services')
         const reviewCollection = client.db('cookingCorner').collection('reviews')
+
+
+        app.post('/jwt', (req, res) => {
+            const user = req.body;
+            const token = jwt.sign(user, process.env.ACCESS_TOKEN_SECRET, { expiresIn: '2d' })
+            res.send({ token })
+        })
 
         app.get('/services', async (req, res) => {
             const query = {}
@@ -42,13 +67,13 @@ async function run() {
             res.send(service);
         })
 
-        app.post('/reviews', async (req, res) => {
+        app.post('/reviews', verifyJWT,  async (req, res) => {
             const review = req.body;
             // console.log(review);
             const result = await reviewCollection.insertOne(review);
             res.send(result);
         })
-        app.patch('/reviews/:id', async (req, res) => {
+        app.patch('/reviews/:id', verifyJWT, async (req, res) => {
             const id = req.params.id;
             const filter = { _id: ObjectId(id) }
             const user = req.body;
@@ -68,13 +93,18 @@ async function run() {
             res.send(result);
         })
 
-        app.get('/reviews/:email', async (req, res) => {
-            const name = req.params.email;
-            // console.log(name);
-            const query = {
-                "$or": [
-                    { "email": { $regex: req.params.email } }
-                ]
+        app.get('/reviews', verifyJWT, async (req, res) => {
+            const decoded = req.decoded;
+            
+            if(decoded.email !== req.query.email) {
+                res.status(403).send({message: 'forbidden access'})
+            }
+
+            let query = {};
+            if(req.query.email) {
+                query = {
+                    email: req.query.email
+                }
             }
             const result = await reviewCollection.find(query).sort({ time: -1 }).toArray()
             res.send(result)
@@ -93,7 +123,7 @@ async function run() {
 
         app.get('/review-one/:id', async (req, res) => {
             const id = req.params.id;
-            console.log(id);
+            // console.log(id);
             const query = { _id: ObjectId(id) }
             // const query = {
             //     "$or": [
@@ -104,14 +134,14 @@ async function run() {
             res.send(gotIt)
         })
 
-        app.delete('/reviews/:id', async (req, res) => {
+        app.delete('/reviews/:id', verifyJWT, async (req, res) => {
             const id = req.params.id;
             const query = { _id: ObjectId(id) }
             const result = await reviewCollection.deleteOne(query);
             res.send(result)
         })
 
-        app.post('/add-service', async (req, res) => {
+        app.post('/add-service', verifyJWT, async (req, res) => {
             const service = req.body;
             const result = await serviceCollection.insertOne(service)
             res.send(result);
